b70889426cd4acef30028a1ea1a5a94373c98358
Subin <subin7799@naver.com>
2022-11-20 01:34:17 +0900

    Edit : 지나는 노드 출력

--- backend/find_path.py
+++ backend/find_path.py
@@ -5,6 +5,7 @@ def dijkstra(graph, start):
   for node in graph:
     distances[node] = [float('inf')]
   distances[start][0] = 0
+  distances[start].append(start)
   queue = []
   heapq.heappush(queue, [distances[start][0], start])
 
@@ -18,17 +19,19 @@ def dijkstra(graph, start):
       distance = current_distance + new_distance
       if distance < distances[new_destination][0]:
         distances[new_destination][0] = distance
+        distances[new_destination] += distances[current_destination][1:]
+        distances[new_destination].append(new_destination)
         heapq.heappush(queue, [distance, new_destination])
     
   return distances
 
 graph = {
-    'A': {'B': 8, 'C': 1, 'D': 2},
-    'B': {},
-    'C': {'B': 5, 'D': 2},
-    'D': {'E': 3, 'F': 5},
-    'E': {'F': 1},
-    'F': {'A': 5}
+    'A': {'B': 2, 'D': 1},
+    'B': {'A': 2, 'C': 3, 'D': 2},
+    'C': {'B': 3, 'F': 5},
+    'D': {'A': 1, 'B': 2, 'E': 1},
+    'E': {'D': 1, 'F': 2},
+    'F': {'C': 5, 'E': 2}
 }
 
 print(dijkstra(graph, 'A'))
