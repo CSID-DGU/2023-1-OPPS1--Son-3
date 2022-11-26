6f0abc7b079da4d5464d3339f0f51e6f31e7cc6f
Subin <subin7799@naver.com>
2022-11-26 20:37:24 +0900

    Edit : 길찾기 오류 수정 및 json 파일로 저장

--- backend/find_path.py
+++ backend/find_path.py
@@ -1,80 +1,124 @@
 import heapq
+import json
 
-def dijkstra(graph, start):
-  distances = {}
-  for node in graph:
-    distances[node] = [float('inf')]
-  distances[start][0] = 0
-  distances[start].append(start)
-  queue = []
-  heapq.heappush(queue, [distances[start][0], start])
+def dijkstra(graph, first, last):
+    distance = {node:[float('inf'), first] for node in graph}
+    distance[first] = [0, first]
+    queue = []
 
-  while queue:
-    current_distance, current_destination = heapq.heappop(queue)
+    heapq.heappush(queue, [distance[first][0], first])
 
-    if distances[current_destination][0] < current_distance:
-      continue
-    
-    for new_destination, new_distance in graph[current_destination].items():
-      distance = current_distance + new_distance
-      if distance < distances[new_destination][0]:
-        distances[new_destination][0] = distance
-        distances[new_destination] += distances[current_destination][1:]
-        distances[new_destination].append(new_destination)
-        heapq.heappush(queue, [distance, new_destination])
-  
-  new_distances = {}
-  for node in list(distances.keys()):
-    node_list = []
-    for i in range(len(distances[node])):
-      if distances[node][i] != node:
-        node_list.append(distances[node][i])
-      else:
-        break
-    node_list.append(node)
-    new_distances[node] = node_list
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
 
-  return new_distances
+        if distance[current_node][0] < current_distance:
+            continue
+
+        for next_node, weight in graph[current_node].items():
+            total_distance = current_distance + weight
+
+            if total_distance < distance[next_node][0]:
+                distance[next_node] = [total_distance, current_node]
+                heapq.heappush(queue, [total_distance, next_node])
+
+    path = last
+    path_output = []
+    path_output.append(last)
+    while distance[path][1] != first:
+        path_output.append(distance[path][1])
+        path = distance[path][1]
+    path_output.append(first)
+    path_output.reverse()
+    path_output.insert(0, distance[last][0])
+    return path_output
 
 graph = {
-    '경영관': {'문화관':11, '사회과학관':57},
-    '과학관': {'명진관':37,'상록원':31, 'K':46, 'J':43},
-    '다향관': {'E':18},
-    '만해관': {'법학관':63, '혜화관':54, 'A':33},
-    '명진관': {'과학관':37, 'B':32},
+    '경영관': {'문화관':11, '사회과학관':57, 'QQ':56},
+    '과학관': {'D':1, 'B':1},
+    '다향관': {'M':5, 'WW':1},
+    '만해관': {'법학관':63, '혜화관':54, 'F':33},
+    '명진관': {'D':37, 'G':32},
     '문화관': {'경영관':11, '사회과학관':76, '학술관':15},
-    '법학관': {'만해관':63, 'A':40, 'G':33, 'H':12},
-    '본관': {'원흥관': 31, 'D':12},
-    '사회과학관': {'경영관':57, '문화관':76, '혜화관':46, '혜화문':68},
-    '상록원': {'과학관':31, 'K':47, '대운동장':26},
-    '신공학관': {'C':88, '원흥관':22},
-    '원흥관': {'본관':31, '신공학관':22, '정보문화관':20, 'N':35},
-    '정보문화관': {'원흥관':20, 'O':28, '학생회관':16},
-    '중앙도서관': {'C':26, 'K':54, '신공학관':64},
-    '학림관': {'체육관':56, 'P':108, '후문':79},
-    '학생회관': {'O':24, '정보문화관':16},
+    '법학관': {'만해관':63, 'P':4},
+    '본관': {'K':12, 'S':15},
+    '사회과학관': {'경영관':57, '문화관':76, 'PP':1, 'QQ':46},
+    '상록원': {'A':1, '대운동장':26},
+    '신공학관': {'KK':25},
+    '원흥관': {'Q':13, 'R':11, 'II':13},
+    '정보문화관': {'VV':5, 'YY':5},
+    '중앙도서관': {'H':12, 'Q':50},
+    '학림관': {'체육관':56, 'X':50, 'EE':65},
+    '학생회관': {'AA':5, 'BB':25},
     '학술관': {'문화관':15},
-    '혜화관': {'만해관':54, '사회과학관':46, '혜화문':75},
-    '혜화문': {'혜화관':75, '사회과학관':68},
-    '대운동장': {'A':129, '상록원':26},
-    '체육관': {'M': 76, '학림관':56},
-    '후문': {'학림관':79, 'O':120, 'P':83},
-    '만해광장': {'N':17},
-    '팔정도': {'A':49, 'B':39, 'C':49, 'D':39, 'E':49, 'F':39, 'G':49, 'H':39},
-    'A': {'만해관':33, '법학관':39, 'B':38, 'H':35, '대운동장':129, '팔정도':49, 'J':61},
-    'B': {'명진관':32, 'A':38, 'C':38, '팔정도':39},
-    'C': {'신공학관':88, 'B':38, 'D':35, '팔정도':49, '중앙도서관':26},
-    'D': {'본관':12, 'C':35, 'E':35, '팔정도':39},
-    'E': {'다향관':18, 'D':35, 'F':38, '팔정도':49, 'M':55},
-    'F': {'E':38, 'G':38, '팔정도':39},
-    'G': {'F':38, '법학관':33, 'H':35, '팔정도':49},
-    'H': {'법학관':12, 'A':35, 'G':35, '팔정도':39},
-    'J': {'A': 61, '과학관':43, 'K':89},
-    'K': {'과학관':46, '상록원':47, '중앙도서관':54, 'J':89},
-    'M': {'E':55, 'N':49, '체육관':76},
-    'N': {'원흥관':35, 'M': 49, 'O':105, '만해광장':17},
-    'O': {'정보문화관':28, '학생회관':24, 'N':105, '후문':120, 'P':46},
-    'P': {'학림관':108, '후문':83, 'O':46}
+    '혜화관': {'만해관':54, 'OO':1},
+    '혜화문': {'PP':85, 'NN':72},
+    '대운동장': {'상록원':26, 'UU':1, 'TT':1},
+    '체육관': {'학림관':56, 'W':37},
+    '후문': {'EE':14},
+    '만해광장': {'XX':1},
+    '팔정도': {'F':49, 'P':39, 'O':49, 'N':39, 'L':49, 'K':39, 'J':49, 'G':39},
+    'A': {'B':31, '상록원':1},
+    'B': {'과학관':1, 'A':31, 'C':16},
+    'C': {'B':16, 'D':46, 'H':54},
+    'D': {'과학관':1, '명진관':37, 'C':46, 'E':43},
+    'E': {'D':43, 'F':61},
+    'F': {'E':61, 'G':38, '팔정도':49, 'P':35, '만해관':33, 'UU':110},
+    'G': {'명진관':32, 'F':38, '팔정도':39, 'J':38},
+    'H': {'C':54, '중앙도서관':12, 'I':16},
+    'I': {'H':16, 'J':14},
+    'J': {'I':14, 'Q':50, 'K':35, '팔정도':49, 'G':38},
+    'K': {'J':35, '본관':12, 'L':35, '팔정도':39},
+    'L': {'K':35, 'S':34, 'T':10, 'M':15, '팔정도':49},
+    'M': {'L':15, '다향관':5, 'N':23},
+    'N': {'팔정도':39, 'M':23, 'O':38},
+    'O': {'팔정도':49, 'N':38, 'LL':43, 'P':35},
+    'P': {'팔정도':39, 'O':35, '법학관':4, 'F':35},
+    'Q': {'R':56, 'J':50, '중앙도서관':5, 'KK':32, '원흥관':13},
+    'R': {'Q':56, '원흥관':11, 'JJ':36, 'S':30},
+    'S': {'본관':15, 'R':30, 'L':34},
+    'T': {'L':10, 'V':35, 'U':10},
+    'U': {'T':10, 'WW':10},
+    'V': {'T':35, 'XX':49, 'W':39},
+    'W': {'V':39, '체육관':37},
+    'X': {'학림관':50, 'Y':35},
+    'Y': {'X':35, 'Z':20},
+    'Z': {'Y':20, 'AA':26},
+    'AA': {'학생회관':5, 'Z':26, 'FF':28},
+    'BB': {'학생회관':25, 'CC':37},
+    'CC': {'BB':37, 'DD':50},
+    'DD': {'CC':50, 'EE':20},
+    'EE': {'학림관':65, 'DD':20, '후문':14},
+    'FF': {'YY':5, 'AA':28, 'GG':20},
+    'GG': {'FF':20, 'HH':35},
+    'HH': {'II':21, 'GG':35, 'JJ':35},
+    'II': {'원흥관':13, 'VV':20, 'HH':21},
+    'JJ': {'R':36, 'HH':35, 'XX':49},
+    'KK': {'신공학관':25, 'Q':32},
+    'LL': {'MM':41, 'O':43},
+    'MM': {'LL':41, 'NN':77},
+    'NN': {'OO':55, 'MM':77, '혜화문':72},
+    'OO': {'혜화관':1, 'NN':55, 'PP':46},
+    'PP': {'OO':46, '혜화문':85, '사회과학관':1},
+    'QQ': {'경영관':56, 'RR':70, '사회과학관':46},
+    'RR': {'QQ':70, 'SS':35},
+    'SS': {'RR':35, 'TT':20},
+    'TT': {'SS':20, '대운동장':1, 'UU':17},
+    'UU': {'대운동장':1, 'TT':17, 'F':110},
+    'VV': {'II':20, '정보문화관':5},
+    'WW': {'다향관':1, 'U':10},
+    'XX': {'만해광장':1, 'V':49, 'JJ':49},
+    'YY': {'정보문화관':5, 'FF':5} 
 }
 
-print(dijkstra(graph, '경영관'))
+node_list = ['경영관', '과학관', '다향관', '만해관', '명진관', '문화관', '법학관', '본관', '사회과학관', '상록원', '신공학관', '원흥관', '정보문화관', '중앙도서관', '학림관', '학생회관', '학술관', '혜화관', '혜화문', '대운동장', '체육관', '후문', '만해광장', '팔정도']
+
+path_all = {}
+for start in node_list:
+    path_start = {}
+    for end in node_list:
+      path_start[end] = dijkstra(graph, start, end)
+    path_all[start] = path_start
+
+file_path = "./frontend/src/path.json"
+with open(file_path, 'w', encoding='utf-8') as outfile:
+    json.dump(path_all, outfile, ensure_ascii=False, indent=4)
