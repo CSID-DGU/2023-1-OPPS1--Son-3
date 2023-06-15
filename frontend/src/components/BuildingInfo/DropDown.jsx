7525799e8a903a946db111f9d64b60b35478ffcd
ZZZINU <nnjin987@gmail.com>
2023-06-15 21:57:52 +0900

    feat: 건물정보 페이지 드롭다운 생성 & 이름순/번호순 버튼 생성

---
+++ frontend/src/components/BuildingInfo/DropDown.jsx
@@ -0,0 +1,77 @@
+import styled, { css } from "styled-components";
+
+const DropDown = ({ isOpen, innerRef, setVal, data, top }) => {
+  const handleInput = (e) => {
+    if (setVal) setVal(e.target.textContent);
+  };
+
+  return (
+    <Dropdown ref={innerRef} isOpen={isOpen} top={top}>
+      <ButtonWrap>
+        <Button>이름순</Button>
+        <Button>번호순</Button>
+      </ButtonWrap>
+      {data.map((item, index) => {
+        return (
+          <DropdownItem
+          
+            key={index}
+            onClick={(e) => {
+              handleInput(e);
+            }}
+          >
+            {item}
+          </DropdownItem>
+        );
+      })}
+    </Dropdown>
+  );
+};
+const Dropdown = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  overflow-y: scroll;
+  max-height: 30em;
+  position: absolute;
+  width: 100%;
+  top: ${(props) => `${props.top}px`};
+  padding: 0;
+  background-color: white;
+  border: 2.5px solid black;
+  border-end-end-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border-top: unset;
+  z-index: 1;
+  cursor: pointer;
+  ${(props) =>
+    !props.isOpen &&
+    css`
+      visibility: hidden;
+    `}
+`;
+const DropdownItem = styled.li`
+  list-style: none;
+  flex: none;
+  padding: 0.75em;
+  &:hover {
+    background-color: rgb(243, 202, 89);
+  }
+`;
+const ButtonWrap =styled.div`
+  display: flex;
+  padding: 0.75em;
+  justify-content: space-around;
+`;
+const Button = styled.button`
+  display: flex;
+  border: none;
+  background-color: transparent;
+  font-size: 14px;
+  color: inherit;
+  cursor: pointer;
+    &:hover {
+    font-weight: bold;
+  }
+`;
+export default DropDown;
