4f99c7f89d209a070f805b9242cb28e0ea9101d7
ZZZINU <nnjin987@gmail.com>
2023-06-16 14:15:48 +0900

    feat: 건물정보 페이지 - 검색창 자동완성 기능

--- frontend/src/components/BuildingInfo/DropDown.jsx
+++ frontend/src/components/BuildingInfo/DropDown.jsx
@@ -3,65 +3,84 @@ import styled, { css } from "styled-components";
 
 const DropDown = ({ isOpen, innerRef, setVal, data, data2, top }) => {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
 
   const handleInput = (e) => {
     let value = e.target.textContent;
 
-    if (!isNaN(value[0]))
-    {
-      value = value.substring(value.indexOf(". ") + 2)
+    if (!isNaN(value[0])) {
+      value = value.substring(value.indexOf(". ") + 2);
     }
 
     if (setVal) setVal(value);
+    setSearchValue(value);
   };
 
-  // 이름순 선택
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setSearchValue(value);
+  };
+
   const selectdataName = () => {
     setSelectedOption(data);
   };
 
-  // 번호순 선택
   const selectdataNum = () => {
     setSelectedOption(data2);
   };
 
   const sort = selectedOption || data;
+  const filteredData = sort.filter((item) =>
+    item.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   return (
     <Dropdown ref={innerRef} isOpen={isOpen} top={top}>
+
+        <SearchInput
+          type="text"
+          value={searchValue}
+          onChange={handleInputChange}
+          placeholder="검색"
+        />
+
       <ButtonWrap>
         <Button onClick={selectdataName}>이름순</Button>
         <Button onClick={selectdataNum}>번호순</Button>
       </ButtonWrap>
-      {sort.map((item, index) => {
-        return (
-          <DropdownItem
-            key={index}
-            onClick={(e) => {
-              handleInput(e);
-            }}
-          >
-            {item}
-          </DropdownItem>
-        );
-      })}
+
+      <DropdownItemList>
+        {filteredData.map((item, index) => {
+          return (
+            <DropdownItem
+              key={index}
+              onClick={(e) => {
+                handleInput(e);
+              }}
+            >
+              {item}
+            </DropdownItem>
+          );
+        })}
+      </DropdownItemList>
     </Dropdown>
   );
 };
+
 const Dropdown = styled.ul`
+  margin-top: 0px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  overflow-y: scroll;
-  max-height: 30em;
   position: absolute;
   width: 100%;
-  top: ${(props) => `${props.top}px`};
   padding: 0;
   background-color: white;
   border: 2.5px solid black;
   border-end-end-radius: 10px;
   border-bottom-left-radius: 10px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
   border-top: unset;
   z-index: 1;
   cursor: pointer;
@@ -71,6 +90,12 @@ const Dropdown = styled.ul`
       visibility: hidden;
     `}
 `;
+
+const DropdownItemList = styled.div`
+  max-height: 30em;
+  overflow-y: scroll;
+`;
+
 const DropdownItem = styled.li`
   list-style: none;
   flex: none;
@@ -79,11 +104,13 @@ const DropdownItem = styled.li`
     background-color: rgb(243, 202, 89);
   }
 `;
+
 const ButtonWrap = styled.div`
   display: flex;
   padding: 0.75em;
   justify-content: space-around;
 `;
+
 const Button = styled.div`
   display: flex;
   border: none;
@@ -95,4 +122,16 @@ const Button = styled.div`
     font-weight: bold;
   }
 `;
+
+const SearchInput = styled.input`
+  box-sizing: border-box;
+  border-radius: 15px;
+  padding: 10px;
+  font-size: 20px;
+  border: 2.8px black solid;
+  margin-bottom: 10px;
+  margin-left: -1%;
+  margin-right: -1%;
+`;
+
 export default DropDown;
