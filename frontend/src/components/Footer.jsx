3f4e17518f07fb7f2fbc920d803992270c600d39
your_name <dlekdms_02@naver.com>
2023-06-17 01:57:05 +0900

    fix: Footer 수정
    제발 최종이어라!!

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components/macro";
+
 const Footer = () => {
-  return <Foot>Team of : Midas of Son</Foot>;
+  return <Foot>Team: Son of Midas</Foot>;
 };
+
 export default Footer;
 
 const Foot = styled.footer`
@@ -9,11 +11,8 @@ const Foot = styled.footer`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   font-size: 15px;
-  color: darkgray;
-  padding: 50px;
   text-align: center;
-  position: fixed;
-  left: 0;
-  bottom: 0;
   width: 100%;
+  bottom: 10px;
+  overflow: hidden;
 `;
