0f23415f12e85faf2755f28e60b694abff5a54d4
LeeHZ <126967574+asd56780@users.noreply.github.com>
2023-06-19 01:59:01 +0900

    fix : 대운동장-상록원 계단 정보 수정

--- makejson/find_shortcut.py
+++ makejson/find_shortcut.py
@@ -210,15 +210,15 @@ for start in dict_key:
                 shortcut.append(sc)
                 
 
-            # 상록원 -> 대운동장
-            elif path_data[start][end][i-1]=="상록원" and path_data[start][end][i]=="대운동장":
+            # 상록원 -> KK
+            elif path_data[start][end][i-1]=="상록원" and path_data[start][end][i]=="KK":
                 sc.append(image_info["상록원1"]["title"])
                 sc.append(image_info["상록원1"]["img"])
                 sc.append(image_info["상록원1"]["info"])
                 shortcut.append(sc)
             
-            # 대운동장 -> 상록원
-            elif path_data[start][end][i-1]=="대운동장" and path_data[start][end][i]=="상록원":
+            # KK -> 상록원
+            elif path_data[start][end][i-1]=="KK" and path_data[start][end][i]=="상록원":
                 sc.append(image_info["상록원2"]["title"])
                 sc.append(image_info["상록원2"]["img"])
                 sc.append(image_info["상록원2"]["info"])
@@ -500,15 +500,15 @@ for start in dict_key:
                 shortcut.append(sc)
                 
 
-            # 상록원 -> 대운동장
-            elif path_data[start][end][i-1]=="상록원" and path_data[start][end][i]=="대운동장":
+            # 상록원 -> KK
+            elif path_data[start][end][i-1]=="상록원" and path_data[start][end][i]=="KK":
                 sc.append(image_info["상록원1"]["title"])
                 sc.append(image_info["상록원1"]["img"])
                 sc.append(image_info["상록원1"]["info"])
                 shortcut.append(sc)
             
-            # 대운동장 -> 상록원
-            elif path_data[start][end][i-1]=="대운동장" and path_data[start][end][i]=="상록원":
+            # KK -> 상록원
+            elif path_data[start][end][i-1]=="KK" and path_data[start][end][i]=="상록원":
                 sc.append(image_info["상록원2"]["title"])
                 sc.append(image_info["상록원2"]["img"])
                 sc.append(image_info["상록원2"]["info"])
